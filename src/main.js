92830bfb36bfe25ae355147c299c0868ceced39b
shirleysosamuro <shirs.sosa@gmail.com>
2019-06-27 15:44:41 -0500

    Plataforma PokeFan tiene HTML estructurado en el login, el DOM funcionando y CSS similar a login de Instagram

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 /* Manejo del DOM */
+const contrasenaCorrecta = 'LABORATORIA';
+const usuarioCorrecto = 'LABORATORIA';
+const inicioSesion = document.getElementById('inicio-sesion');
+const usuario= document.getElementById('usuario');
+const contrasena= document.getElementById('contrasena');
+const ingresa =document.getElementById ('btt-ingresa');
+const listaDePokemones = document.getElementById('lista-de-pokemones');
+
+let nroIntentos = 0;  
+const validar = () => {
+    if (contrasena.value === contrasenaCorrecta && usuario.value === usuarioCorrecto) {
+    inicioSesion.classList.add('hide');
+    listaDePokemones.classList.replace('hide', 'show');
+    event.preventDefault();    
+    } else {
+    if (nroIntentos < 1) {
+        contrasena.value = '';
+        nroIntentos++;
+    }
+    }
+    };
+    
+    ingresa.addEventListener('click', validar);
+
+    
+     
+   
