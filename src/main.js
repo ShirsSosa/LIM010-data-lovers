3031261bc78e2dec87715b61c28c2f62e487850d
shirleysosamuro <shirs.sosa@gmail.com>
2019-06-27 23:11:52 -0500

    Volviendo el js a como estaba

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
-let password = 0;
-const btningresa = document.getElementById('btningresa');
-btningresa.addEventListener("click", () => {
-    if (password <= 3) {
-        const contrasena = document.getElementById('contrasena').value;
-        const usuario = document.getElementById('usuario').value
-        if (contrasena == 'LABORATORIA' && usuario == 'LABORATORIA') {
-            document.getElementById('iniciosesion').classList.add('hide');
-            document.getElementById('data').classList.remove('hide');
-        } else {
-            document.getElementById("error").innerHTML = `Contraseña incorrecta, intente de nuevo <i class="fas fa-exclamation-circle"></i>`;
-            password = password + 1;
-            document.getElementById('contrasena').value = "";
-            if (password > 2) {
-                document.getElementById("error").innerHTML = `Numero de intentos excedidos, regrese en 6 horas <i class="fas fa-exclamation-circle"></i>`;
-                document.getElementById('contrasena').disabled = true;
-                document.getElementById('bttingresa').disabled = true;
-            }
-        }
+const contrasenaCorrecta = 'LABORATORIA';
+const usuarioCorrecto = 'LABORATORIA';
+const inicioSesion = document.getElementById('inicio-sesion');
+const usuario= document.getElementById('usuario');
+const contrasena= document.getElementById('contrasena');
+const ingresa =document.getElementById ('btt-ingresa');
+const listaDePokemones = document.getElementById('lista-de-pokemones');
+
+ let nroIntentos = 0;  
+const validar = () => {
+    if (contrasena.value === contrasenaCorrecta && usuario.value === usuarioCorrecto) {
+    inicioSesion.classList.add('hide');
+    listaDePokemones.classList.replace('hide', 'show');
+    event.preventDefault();    
+    } else {
+    if (nroIntentos < 1) {
+        contrasena.value = '';
+        nroIntentos++;
     }
-});
+    }
+    };
+
+     ingresa.addEventListener('click', validar);
