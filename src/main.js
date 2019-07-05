c3c81b4490f60c9302ecb43ae3fc9f7907c4457c
shirleysosamuro <shirs.sosa@gmail.com>
2019-07-05 13:00:51 -0500

    Hemos arreglado el término de los ID

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
+const poke = POKEMON.pokemon; //array
 const contrasenaCorrecta = 'LABORATORIA';
 const usuarioCorrecto = 'LABORATORIA';
 const inicioSesion = document.getElementById('inicio-sesion');
 const usuario = document.getElementById('usuario');
 const contrasena = document.getElementById('contrasena');
 const ingresa = document.getElementById('btt-ingresa');
-const listaDePokemones = document.getElementById('lista-de-pokemones'); //en la segunda pantalla, despues e login
-const poke = POKEMON.pokemon; //array
-const todosPokemones = document.getElementById('mostrar-pokemones'); //contenedor de pokemones
+const pantallaPrincipal = document.getElementById('pantalla-principal'); //en la segunda pantalla, despues e login
+const todosPokemones = document.getElementById('mostrar-pokemones'); //contenedor de pokemones//
 const mostrarHeader = document.getElementById('header');
 const mostrarFooter = document.getElementById('footer');
-const filtrado = document.getElementById('filtrado');
+const filtrado = document.getElementById('filtrado');//selec az y za//
+//pantalla login: validación de clave, una vez confirmada, ingresas a la pantalla principal//
 let nroIntentos = 0;
 const validar = () => {
   if (
@@ -17,7 +18,7 @@ const validar = () => {
     usuario.value === usuarioCorrecto
   ) {
     inicioSesion.classList.add('hide');
-    listaDePokemones.classList.remove('hide');
+    pantallaPrincipal.classList.remove('hide');
     mostrarHeader.classList.remove('hide');
     mostrarFooter.classList.remove('hide');
     event.preventDefault();
@@ -29,7 +30,7 @@ const validar = () => {
   }
 };
 ingresa.addEventListener('click', validar);
-/* Con esta funcion ya tengo solo el nombre y la imagen de todos mis pokemones*/
+/* pprincipal: Con esta funcion ya tengo  el nombre y la imagen de todos mis pokemones en div*/
 const dataPokemones = mostrarDatosPokemon(poke);
 let mostrar = '';
 for (let i = 0; i < dataPokemones.length; i++) {
@@ -40,7 +41,7 @@ for (let i = 0; i < dataPokemones.length; i++) {
  <h2>${dataPokemones[i].numero}</h2>
  </div>`;
 }
-/* Función para ordenar de la AZ*/
+/*pprincipal: Función para ordenar de la AZ*/
 const filtradoAZ = () => {
   const namePoke = [];
   const abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -56,7 +57,6 @@ const filtradoAZ = () => {
 filtrado.addEventListener('change', (event) => {
   if ('A-Z' === filtrado.value) {
     const orden = filtradoAZ();
-    document.getElementById('demo').innerHTML = dataPokemones(orden);
-    
+    todosPokemones.innerHTML = dataPokemones(orden);
   }
 });
