b4e80645c93bc249d5b4a8272da7e161c1d8533c
shirleysosamuro <shirs.sosa@gmail.com>
2019-07-05 08:34:39 -0500

    h2 completada, usuario puede ingresa a pantalla 2 y ver a lso 151 pokemones

--- src/main.js
+++ src/main.js
@@ -4,56 +4,59 @@ const inicioSesion = document.getElementById('inicio-sesion');
 const usuario = document.getElementById('usuario');
 const contrasena = document.getElementById('contrasena');
 const ingresa = document.getElementById('btt-ingresa');
-const listaDePokemones = document.getElementById('lista-de-pokemones');//en la segunda pantalla, despues e login
-const poke= POKEMON.pokemon; //array
-const todosPokemones = document.getElementById('mostrar-pokemones');//contenedor de pokemones
+const listaDePokemones = document.getElementById('lista-de-pokemones'); //en la segunda pantalla, despues e login
+const poke = POKEMON.pokemon; //array
+const todosPokemones = document.getElementById('mostrar-pokemones'); //contenedor de pokemones
 const mostrarHeader = document.getElementById('header');
 const mostrarFooter = document.getElementById('footer');
-const filtrado=document.getElementById('filtrado');
+const filtrado = document.getElementById('filtrado');
 let nroIntentos = 0;
 const validar = () => {
-    if (contrasena.value === contrasenaCorrecta && usuario.value === usuarioCorrecto) {
-        inicioSesion.classList.add('hide');
-        listaDePokemones.classList.remove('hide');
-        mostrarHeader.classList.remove('hide');
-        mostrarFooter.classList.remove('hide');
-        event.preventDefault();
-    } else {
-        if (nroIntentos < 1) {
-            contrasena.value = '';
-            nroIntentos++;
-        }
+  if (
+    contrasena.value === contrasenaCorrecta &&
+    usuario.value === usuarioCorrecto
+  ) {
+    inicioSesion.classList.add('hide');
+    listaDePokemones.classList.remove('hide');
+    mostrarHeader.classList.remove('hide');
+    mostrarFooter.classList.remove('hide');
+    event.preventDefault();
+  } else {
+    if (nroIntentos < 1) {
+      contrasena.value = '';
+      nroIntentos++;
     }
+  }
 };
 ingresa.addEventListener('click', validar);
 /* Con esta funcion ya tengo solo el nombre y la imagen de todos mis pokemones*/
 const dataPokemones = mostrarDatosPokemon(poke);
 let mostrar = '';
-for(let i = 0; i < dataPokemones.length; i++){
- todosPokemones.innerHTML += `
+for (let i = 0; i < dataPokemones.length; i++) {
+  todosPokemones.innerHTML += `
  <div class='mostrar'>
  <h1>${dataPokemones[i].nombre}</h1> 
- <img id=${i+1} src='${dataPokemones[i].imagen}'/>
+ <img id=${i + 1} src='${dataPokemones[i].imagen}'/>
  <h2>${dataPokemones[i].numero}</h2>
  </div>`;
-};
+}
 /* Función para ordenar de la AZ*/
 const filtradoAZ = () => {
-    const namePoke=[];
-    const abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    for(let i = 0; i < abc.length; i++){
-    for(let j = 0; j < poke.length; j++){
-        if (abc[i]=== poke[j].name[0]){
-            namePoke.push(poke[j]);
-        }
+  const namePoke = [];
+  const abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  for (let i = 0; i < abc.length; i++) {
+    for (let j = 0; j < poke.length; j++) {
+      if (abc[i] === poke[j].name[0]) {
+        namePoke.push(poke[j]);
+      }
     }
-}  
-        return namePoke;
+  }
+  return namePoke;
 };
-filtrado.addEventListener('change',(event)=>{
- if('A-Z' === filtrado.value){
-  const orden = filtradoAZ();
-  console.log(poke);
-  listaDePokemones.innerHTML = poke(orden);     
- }});
-
+filtrado.addEventListener('change', (event) => {
+  if ('A-Z' === filtrado.value) {
+    const orden = filtradoAZ();
+    document.getElementById('demo').innerHTML = dataPokemones(orden);
+    
+  }
+});
