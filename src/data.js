c99d3599f84fad2ecbcc13788e7c10b01e5e6fdb
shirleysosamuro <shirs.sosa@gmail.com>
2019-07-10 06:43:15 -0500

    se ordena por filtro, debilidad

--- src/data.js
+++ src/data.js
@@ -1,63 +1,75 @@
 const mostrarDatosPokemon = (array) => {
     const newArrayDataPokemones = [];
     for (let i = 0; i < array.length; i++) {
-        newArrayDataPokemones.push({
-            id : array[i].id,
-            nombre: array[i].name,
-            imagen: array[i].img,
-            numero: array[i].num,
-        });
+      newArrayDataPokemones.push({
+        id: array[i].id,
+        nombre: array[i].name,
+        imagen: array[i].img,
+        numero: array[i].num,
+      });
     };
     return newArrayDataPokemones;
-};
-
-window.mostrarDatosPokemon = mostrarDatosPokemon;
-
-//Función para mostrar caracteristica de pokemon en general
-
-//Función para ordenar de la AZ
-const sortAZ = (array, clickOrder) => {
+  };
+  
+  window.mostrarDatosPokemon = mostrarDatosPokemon;
+  
+  //Función para mostrar caracteristica de pokemon en general
+  
+  //Función para ordenar de la AZ
+  const sortAZ = (array, clickOrder) => {
     const sortName = array.sort((a, b) => {
-        if (a.name > b.name) {
-            return 1;
-        }
-        if (a.name < b.name) {
-            return -1;
-        }
-        return 0;
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
     });
     if (clickOrder === '0') {
-        return sortName;
+      return sortName;
     }
     if (clickOrder === '1') {
-        return sortName.reverse();
+      return sortName.reverse();
     }
     return 0;
-};
-window.sortAZ = sortAZ;
-
-const sortSpawn = (array, clickOrder) => {
+  };
+  window.sortAZ = sortAZ;
+  
+  const sortSpawn = (array, clickOrder) => {
     const sortNum = array.sort((a, b) => {
-        if (a.spawn_chance > b.spawn_chance) {
-            return 1;
-        }
-        if (a.spawn_chance < b.spawn_chance) {
-            return -1;
-        }
-        return 0;
+      if (a.spawn_chance > b.spawn_chance) {
+        return 1;
+      }
+      if (a.spawn_chance < b.spawn_chance) {
+        return -1;
+      }
+      return 0;
     });
     if (clickOrder === '0') {
-        return sortNum;
+      return sortNum;
     }
     if (clickOrder === '1') {
-        return sortNum.reverse();
+      return sortNum.reverse();
     }
     return 0;
-};
-window.sortSpawn = sortSpawn;
-
- const tipoPoke = (array, condition) => {
-     const filtroTipoPoke = array.filter(caracter => (caracter.type[0] === condition || caracter.type[1] === condition));
-     return filtroTipoPoke;
-}; 
-window.tipoPoke=tipoPoke;
+  };
+  window.sortSpawn = sortSpawn;
+  //Filtrando objeto por tipo
+  const tipoPoke = (array, condition) => {
+    const filtroTipoPoke = array.filter(caracter => (caracter.type[0] === condition || caracter.type[1] === condition));
+    return filtroTipoPoke;
+  };
+  window.tipoPoke = tipoPoke;
+  //Filtrando por huevo
+  const huevoPoke = (array, condition) => {
+    const filtroHuevoPoke = array.filter(caracter => (caracter.egg[0] === condition || caracter.egg[1] === condition));
+    return filtroHuevoPoke;
+  };
+  window.huevoPoke = huevoPoke;
+  //Filtrando por debilidad
+  const debilidadPoke = (array,condition) => {
+    const filtroDebilidadPoke = array.filter(caracter => (caracter.weaknesses[0] === condition || caracter.weaknesses[1] === condition || caracter.weaknesses[2] === condition || caracter.weaknesses[3] === condition || caracter.weaknesses[4] === condition || caracter.weaknesses[5] === condition || caracter.weaknesses[6] === condition));
+    return filtroDebilidadPoke;
+  };
+  window.debilidadPoke = debilidadPoke;
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const ingresa = document.getElementById('btt-ingresa');
 const mostrarHeader = document.getElementById('header');
 const mostrarFooter = document.getElementById('footer');
 const listaDePokemones = document.getElementById('pantalla-principal');//en la segunda pantalla, despues e login
-const poke= POKEMON.pokemon; //array
+const poke = POKEMON.pokemon; //array
 const todosPokemones = document.getElementById('mostrar-pokemones');//contenedor de pokemones
 const close = document.getElementById('close');
 
@@ -27,53 +27,182 @@ const validar = () => {
     }
 };
 ingresa.addEventListener('click', validar);
- /* pprincipal: Con esta funcion ya tengo  el nombre y la imagen de todos mis pokemones en div*/
- const dataPokemones = (pokemon) => {
-     let mostrar = ' '; 
-     for (let i = 0 ; i < pokemon.length ; i++) {
-         let llamar = `
+/* pprincipal: Con esta funcion ya tengo  el nombre y la imagen de todos mis pokemones en div*/
+const dataPokemones = (pokemon) => {
+    let mostrar = ' ';
+    for (let i = 0; i < pokemon.length; i++) {
+        let llamar = `
          <div class='mostrar' name='pokemon' id=${pokemon[i].id}>
          <h1>${pokemon[i].name}</h1> 
          <img src='${pokemon[i].img}'/>
          <h2>${pokemon[i].num}</h2>
          </div>`;
-         mostrar += llamar;
-         }
-         return mostrar;
+        mostrar += llamar;
+    }
+    return mostrar;
 };
-todosPokemones.innerHTML = dataPokemones(poke); 
- //Función para ordenar de la ordenar alfabeticamente
- const sortAbc = document.getElementById('filtrado-az');
- sortAbc.addEventListener('change',() => {
-     const pokeOrden = sortAZ(poke,sortAbc.value);
-     todosPokemones.innerHTML =  dataPokemones(pokeOrden);
- });
+todosPokemones.innerHTML = dataPokemones(poke);
+//Función para ordenar de la ordenar alfabeticamente
+const sortAbc = document.getElementById('filtrado-az');
+sortAbc.addEventListener('change', () => {
+    const pokeOrden = sortAZ(poke, sortAbc.value);
+    todosPokemones.innerHTML = dataPokemones(pokeOrden);
+});
 //Función para ordenar de la ordenar por spwan
 const sortNumSpawn = document.getElementById('filtrado-spawn');
-sortNumSpawn.addEventListener('change',() => {
-    const pokeOrden = sortSpawn(poke,sortNumSpawn.value);
-    todosPokemones.innerHTML =  dataPokemones(pokeOrden);
+sortNumSpawn.addEventListener('change', () => {
+    const pokeOrden = sortSpawn(poke, sortNumSpawn.value);
+    todosPokemones.innerHTML = dataPokemones(pokeOrden);
 });
 //mostrar mi pokemones en el modal
-todosPokemones.addEventListener('click',() =>{ //crear un evento en base a click en cada poke
-    const pokemoncito = event.target.parentElement.id-1;
-    if(event.target.parentElement.getAttribute('name') === 'pokemon'){
-    //mostrando modal 
-    document.getElementById('my-modal').classList.remove('hide');
-    //insertando info-poke en modal
-    document.getElementById('info-de-poke').innerHTML =  `
+todosPokemones.addEventListener('click', () => { //crear un evento en base a click en cada poke
+    const pokemoncito = event.target.parentElement.id - 1;
+    if (event.target.parentElement.getAttribute('name') === 'pokemon') {
+        //mostrando modal 
+        document.getElementById('my-modal').classList.remove('hide');
+        //insertando info-poke en modal
+        document.getElementById('info-de-poke').innerHTML = `
     <img class="imagenModal" src="${POKEMON.pokemon[pokemoncito].img}"/>
     <p class='nombrePokemoncito'> ${POKEMON.pokemon[pokemoncito].name}</p>
-     <p>Peso: ${POKEMON.pokemon[pokemoncito].weight}</p> 
-    <p>Altura: ${POKEMON.pokemon[pokemoncito].height}</p>    
-    <p>Tipo: ${POKEMON.pokemon[pokemoncito].type}</p> `;
+    <div id="myProgress">
+      <div id="myBar"></div>
+    </div>
+    <div>
+      <div>
+        <p>Peso: ${POKEMON.pokemon[pokemoncito].weight}</p> 
+        <p>Altura: ${POKEMON.pokemon[pokemoncito].height}</p>
+      </div>    
+      <p>Tipo: ${POKEMON.pokemon[pokemoncito].type}</p> 
+      <p>Huevo: ${POKEMON.pokemon[pokemoncito].egg}</p>
+      <p>Debilidades: ${POKEMON.pokemon[pokemoncito].weaknesses}</p>
+    </div>`;
     }
-    });
+});
 //cerrando modal (funcion)
-close.addEventListener('click',() => {
+close.addEventListener('click', () => {
     document.getElementById('my-modal').classList.add('hide');
-});   
+});
 //Filtrar por tipo
+const agua = document.getElementById('agua');
+agua.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Water');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const fuego = document.getElementById('fuego');
+fuego.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Fire');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const planta = document.getElementById('planta');
+planta.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Grass');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const roca = document.getElementById('roca');
+roca.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Rock');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const tierra = document.getElementById('tierra');
+tierra.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Ground');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const aire = document.getElementById('aire');
+aire.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Flying');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const dragon = document.getElementById('dragon');
+dragon.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Dragon');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const electrico = document.getElementById('electrico');
+electrico.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Electric');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const fantasma = document.getElementById('fantasma');
+fantasma.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Ghost');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const hielo = document.getElementById('hielo');
+hielo.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Ice');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const insecto = document.getElementById('insecto');
+insecto.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Bug');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const lucha = document.getElementById('lucha');
+lucha.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Fighting');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const normal = document.getElementById('normal');
+normal.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Normal');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const psiquico = document.getElementById('psiquico');
+psiquico.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Psychic');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
+const veneno = document.getElementById('veneno');
+veneno.addEventListener('click', () => {
+    let vacio = '';
+    let arrar = [];
+    arrar = tipoPoke(poke, 'Poison');
+    vacio = dataPokemones(arrar);
+    todosPokemones.innerHTML = vacio;
+});
 /*tipopoke.addEventListener('change',() => {
     const anis = document.getElementById('tipopoke').value;
     let aguita = '';
@@ -82,118 +211,20 @@ close.addEventListener('click',() => {
     aguita = dataPokemones(arrar);
     todosPokemones.innerHTML = aguita;
 });*/
-const agua = document.getElementById('agua');
-agua.addEventListener('click', () => {
-    let vacio = '';
-    let arrar = [];
-    arrar = tipoPoke(poke,'Water');
-    vacio= dataPokemones(arrar);
-    todosPokemones.innerHTML = vacio;
+filtrohuevo.addEventListener('change', () => {
+    const huevoKm = document.getElementById('filtrohuevo').value;
+    let stringVacio = '';
+    let arrayVacio = [];
+    arrayVacio = huevoPoke(poke, huevoKm);
+    stringVacio = dataPokemones(arrayVacio);
+    todosPokemones.innerHTML = stringVacio;
 });
-const fuego = document.getElementById('fuego');
-fuego.addEventListener('click', () => {
+//filtro por debilidad en select
+debilidad.addEventListener('change', () => {
+    const debTipo = document.getElementById('debilidad').value;
     let vacio = '';
     let arrar = [];
-    arrar = tipoPoke(poke,'Fire');
+    arrar = debilidadPoke(poke, debTipo);
     vacio = dataPokemones(arrar);
     todosPokemones.innerHTML = vacio;
 });
-const planta = document.getElementById('planta');
-planta.addEventListener('click', () => {
-    let vacio = '';
-    let arrar = [];
-    arrar = tipoPoke(poke,'Grass');
-    vacio = dataPokemones(arrar);
-    todosPokemones.innerHTML = vacio;
-});
-const roca = document.getElementById('roca');
-roca.addEventListener('click', () => {
-    let vacio = '';
-    let arrar = [];
-    arrar = tipoPoke(poke,'Rock');
-    vacio = dataPokemones(arrar);
-    todosPokemones.innerHTML = vacio;
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const pintarPokemonesEnPantala = (pokemones) => {
-//     let mostrar = ' ';
-//     for (let i = 0; i < pokemon.length; i++) {
-//         let llamar = ` 
-//         <div class="mostrar">
-//         <p> ${pokemon[i].num}</p>
-//         <img src="${pokemon[i].img}"/>
-//         <p> ${pokemon[i].name}</p>
-//         <p> ${pokemon[i].type}</p>
-
-//         </div> `;
-//         mostrar += llamar;
-//     }
-//     return mostrar;
-// };
-// )
-// pintarPokemonesEnPantala(
-// todosPokemones.innerHTML = mostrarData(poke); */
-
-/* const targetPoke = (pokemon) => {
-    let mostrar = ' ';
-    for (let i = 0; i < pokemon.length; i++) {
-        for(let j = 0; j < pokemon[i].id.length; j++){
-            if( pokemon[i].id[j] === ''){
-                let llamar = ` 
-        <div class="mostrar">
-        <p> ${pokemon[i].num}</p>
-        <img src="${pokemon[i].img}"/>
-        <p> ${pokemon[i].name}</p>
-        <p> ${pokemon[i].type}</p>
-
-        </div> `;
-        mostrar += llamar;
-           }   }
-    }
-    return newArrayData;
-} 
-const mostrarTarget = document.getElementById('show-modal');
-pokemon.target.innerHTML = targetPoke(poke); */
-
-
-/* 
-const mostrarPokeAgua = (pokemon) => {
-    let mostrar = ' ';
-    let array = [];
-    array = mostrarAgua();
-    console.log(array);
-    for (let i = 0; i < pokemon.length; i++) {  
-        let llamar = ` 
-        <div class="mostrar">
-        <p> ${pokemon[i].type}</p>
-        </div> `;
-        mostrar += llamar;
-    }
-    return mostrar;
-};
- */
-/* const agua = document.getElementById('agua');
-agua.addEventListener('click', () => {
-    agua.innerHTML = mostrarPokeAgua(poke);
-}); */
